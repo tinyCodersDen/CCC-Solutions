e3378f68ef41477b60c2f5a22f6ba722394eb8d7
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2021-08-04 15:56:17 -0400

    Create J4.py

---
+++ 2012/Junior Problems/J4.py
@@ -0,0 +1,19 @@
+# The problem name is "Big Bang Secrets"
+import string
+k = int(input(''))
+string1 = input('')
+list1 = list(string.ascii_uppercase)
+p = 1
+newstring = ''
+for i in string1:
+    s = 3*p+k
+    try:
+        new = list1[list1.index(i)-s]
+        newstring+=new
+        p+=1
+    except IndexError:
+        s = 26-(list1.index(i)+1)
+        new = list1[s]
+        newstring+=new
+        p+=1
+print(newstring)
