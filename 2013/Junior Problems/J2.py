42bf8994eb509ea8dfd00dc9862d958a7fbc7300
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2021-08-04 15:38:50 -0400

    Create J2.py

---
+++ 2013/Junior Problems/J2.py
@@ -0,0 +1,10 @@
+# The problem name is "Rotating letters"
+list1 = ["I","O","S","H",'Z',"X","N"]
+ask = input('')
+for a in ask:
+    if a in list1:
+        pass
+    else:
+        print('NO')
+        exit()
+print('YES')
