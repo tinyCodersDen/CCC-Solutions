20632211c7f4f795f26b18ac142b64c5a9a71bd5
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2021-08-04 15:48:40 -0400

    Create J1.py

---
+++ 2011/Junior Problems/J1.py
@@ -0,0 +1,11 @@
+# The problem name is "Which Alien?"
+print('How many antennas?')
+ask1 = int(input(''))
+print('How many eyes?')
+ask2 = int(input(''))
+if ask1>=3 and ask2<=4:
+    print('TroyMartian')
+if ask1<=6 and ask2>=2:
+    print('VladSaturnian')
+if ask1<=2 and ask2<=3:
+    print('GraemeMercurian')
