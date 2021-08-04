26e2bc6de39786105ea9a8fb81e269fdf0324bfb
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2021-08-04 15:58:09 -0400

    Create J3.py

---
+++ 2012/Junior Problems/J3.py
@@ -0,0 +1,18 @@
+# The problem name is "Icon Scaling"
+string1,string2,string3 = '','',''
+ask = int(input(''))
+string1+='*'*ask
+string1+='x'*ask
+string1+='*'*ask
+string2+=' '*ask
+string2+='x'*ask
+string2+='x'*ask
+string3+='*'*ask
+string3+=' '*ask
+string3+='*'*ask
+for j in range(ask):
+    print(string1)
+for j in range(ask):
+    print(string2)
+for j in range(ask):
+    print(string3)
