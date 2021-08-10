31a509af18d83826cc8d74a19d5724e6194f8ca7
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2021-08-10 16:33:22 -0400

    Create J1.py

---
+++ 2020/Junior Problems/J1.py
@@ -0,0 +1,9 @@
+list1 = []
+for x in range(3):
+    ask = int(input(''))
+    list1.append(ask)
+var1 = (1*list1[0])+(2*list1[1])+(3*list1[2])
+if var1>=10:
+    print('happy')
+else:
+    print('sad')
