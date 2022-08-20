8eef0bb26c8e27c43f0f7085c235398acb6e8b3b
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2022-08-20 14:47:38 -0400

    Create J2.py

---
+++ 2007/Python/Junior Problems/J2.py
@@ -0,0 +1,18 @@
+while True:
+    txtmsg = input('')
+    if txtmsg=='CU': print('see you')
+    elif txtmsg==':-)': print("I'm happy")
+    elif txtmsg==':-(': print("I'm unhappy")
+    elif txtmsg==';-)': print("wink")
+    elif txtmsg==':-P': print("stick out my tongue")
+    elif txtmsg=='(~.~)': print("sleepy")
+    elif txtmsg=='TA': print("totally awesome")
+    elif txtmsg=='CCC': print("Canadian Computing Competition")
+    elif txtmsg=='CUZ': print("because")
+    elif txtmsg=='TY': print("thank-you")
+    elif txtmsg=='YW': print("you're welcome")
+    elif txtmsg=='TTYL': 
+        print("talk to you later")
+        break
+    else:
+        print(txtmsg)
