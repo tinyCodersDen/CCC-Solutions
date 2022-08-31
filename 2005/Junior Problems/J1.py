2d7591aa43a1439d8cd4aace0baf152213cfa1a7
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2022-08-31 14:41:16 -0400

    Update J1.py

--- 2005/Junior Problems/J1.py
+++ 2005/Junior Problems/J1.py
@@ -9,11 +9,19 @@ if Bc<=0:
     Bc = 0
 A = ((Ac)+(15*Evening)+(20*weekend))/100
 B = ((Bc)+(35*Evening)+(25*weekend))/100
+pa = str(A).split('.')
+pb = str(B).split('.')
+if len(pa[1])==1:
+    pa[1]+='0'
+if len(pb[1])==1:
+    pb[1]+='0'
+A = '.'.join(pa)
+B = '.'.join(pb)
 print('Plan A costs',A)
 print('Plan B costs',B)
-if A<B:
+if float(A)<float(B):
     print('Plan A is cheapest.')
-elif A>B:
+elif float(A)>float(B):
     print('Plan B is cheapest.')
 else:
     print('Plan A and B are the same price.')
