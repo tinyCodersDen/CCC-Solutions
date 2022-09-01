887337c83ccf2fa45f5025acbd029ff94ef3ce26
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2022-09-01 16:43:01 -0400

    Create J4.py

---
+++ 2004/Python/Junior Problems/J4.py
@@ -0,0 +1,22 @@
+import string
+cipher = input('')
+encoder = input('')
+cipher2 = []
+l = list(string.ascii_lowercase)
+for zxc in list(string.ascii_lowercase):
+  l.append(zxc)
+encoded_message = ''
+final_text = ''
+for z in cipher.lower():
+  cipher2.append(ord(z)-97)
+encoder = encoder.lower()
+for q in encoder:
+  if q in string.ascii_lowercase:
+    encoded_message+=q
+c = 0
+for i in encoded_message:
+  final_text+=l[l.index(i)+cipher2[c]]
+  c+=1
+  if c==len(cipher2):
+    c=0
+print(final_text.upper())
