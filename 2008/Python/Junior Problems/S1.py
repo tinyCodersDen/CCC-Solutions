ce932c87ef4bbb457e81a3cbd5a1384e962b2196
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2022-09-01 16:50:56 -0400

    Delete S1.py

--- 2008/Python/Junior Problems/S1.py
+++
@@ -1,9 +0,0 @@
-a1,a = '',1000000000
-while True:
-    k = input('').split(' ')
-    if int(k[1])<a:
-        a = int(k[1])
-        a1 = k[0]
-    if k[0]=='Waterloo':
-        print(a1)
-        break
